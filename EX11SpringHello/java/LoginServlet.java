59eb8f97bccfd129830aedb5c3fde8b3c43182f5
newgitact <asdf72424293@gmail.com>
2022-04-29 00:26:26 +0530

    1

---
+++ EX11SpringHello/java/LoginServlet.java
@@ -0,0 +1,67 @@
+//package com.session;
+
+import java.io.IOException;  
+import java.io.PrintWriter;  
+  
+import javax.servlet.ServletException;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import javax.servlet.http.HttpSession; 
+
+
+import javax.servlet.annotation.WebServlet;
+ 
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+		response.setContentType("text/html");  
+		PrintWriter out=response.getWriter();  
+          
+        String name=request.getParameter("name");  
+        String password=request.getParameter("password");  
+          
+        if(password.equals("admin123") && name.equals("admin")){
+        	request.getRequestDispatcher("link.html").include(request, response);  
+	        out.print("Welcome, "+name);  
+        }  
+        else{  
+            out.print("Sorry, username or password error!");  
+            request.getRequestDispatcher("login.html").include(request, response);  
+        }  
+        
+        
+        
+        
+    }  
+		
+}
+
+
